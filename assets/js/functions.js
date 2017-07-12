0f0a40abb2576c2dde049c72855c3455ce01a0ec
bobbidigi <bobbidigi34@gmail.com>
2017-07-12 13:22:21 -0400

    window mobileAnimation meh??? not satisfied

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -62,6 +62,56 @@ function  workLoad() {
 
 
 
+// Returns true if the specified element has been scrolled into the viewport.
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.thumb-overlay');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+        
+    if (isElementInViewport($elem)) {
+        // Start the animation
+    setTimeout(function() {
+       $elem.addClass('start');
+    },1000);
+        
+    setTimeout(function() {
+       $elem.removeClass('start');
+    },5000);
+        
+}
+}
+
+
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
+
+/*enter: function() {
+        $elem.addClass('start');
+    },
+    exit: function() {  // optionally
+        $elem.removeClass('start');
+    }*/
 
 function clientStuff() {
 
@@ -416,4 +466,8 @@ function clientStuff() {
 			adjust();
 		});
 	};
+        
 }(jQuery || $)); // jQuery or jQuery-like library, such as Zepto
+
+
+
