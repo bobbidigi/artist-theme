28337992d363c39ec4f02f0b1927fca0708b9af0
bobbidigi <bobbidigi34@gmail.com>
2017-07-13 20:20:03 -0400

    fadeout-thumb overlay

--- assets/js/waypoints.js
+++ assets/js/waypoints.js
@@ -1,9 +1,17 @@
+//adds animate-thumb class when in view
+//adds fadeout-thumb after 5 s
 var $thumbOverlay = $('.thumb-overlay');
 
 $thumbOverlay.waypoint(function (direction) {
   if (direction == 'down') {
     $thumbOverlay.addClass('animate-thumb');
+    setTimeout(function() {
+    $thumbOverlay.addClass('fadeout-thumb');
+    },5000);  
   } else {
-    $thumbOverlay.removeClass('animate-thumb'); 
+    $thumbOverlay.removeClass('animate-thumb');  
+    $thumbOverlay.removeClass('fadeout-thumb');  
   }
+     
 }, { offset: '25%' });
+
